0a4b7471eba3241d0cb247ef970c243fd6876017
Matteo <grecomatteo86@gmail.com>
2021-05-24 17:41:48 +0200

    aggiunta commenti su post published e creazione newComment

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -12,10 +12,27 @@ class CommentsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         // 1. seleziono solo i posts pubblicati
         $posts = Post::where('published', 1)->get();
-        dd($posts);
+
+        // 2. ciclo sui posts pubblicati per generare i commenti random (da 1 a 3)
+        foreach($posts as $post){
+            // 3.  Non tutti i post devono per forza avere dei commenti, possono anche non averne (se i = 0)
+            for($i = 0; $i < rand(0, 3); $i++){
+
+                $newComment = new Comment();
+
+                $newComment->post_id = $post->id;
+                $newComment->name = $faker->name();
+                $newComment->content = $faker->text();
+
+                $newComment->save();
+
+            }
+
+        }
+     
     }
 }
