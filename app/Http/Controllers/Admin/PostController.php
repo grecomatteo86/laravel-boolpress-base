7e60152d3b6c5dc7ce7cbaef49c84c13525b7eca
Matteo <grecomatteo86@gmail.com>
2021-05-25 23:10:52 +0200

    fix store

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Post;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
