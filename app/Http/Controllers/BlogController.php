a9918a669fff0aa71ea5ca067e16ce229df91ff5
Matteo <grecomatteo86@gmail.com>
2021-05-25 23:55:26 +0200

    imposto la show in BlogController

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -15,4 +15,17 @@ class BlogController extends Controller
         return view('guest.index', compact('posts'));
 
     }
+
+    public function show($slug)
+    {
+        // prendo i dati dal db
+        $post = Post::where('slug', $slug)->first();
+        
+        if ( $post == null ) {
+            abort(404);
+        }
+        // restituisco la pagina del post
+        return view('guest.show', compact('post'));
+    }
+
 }
