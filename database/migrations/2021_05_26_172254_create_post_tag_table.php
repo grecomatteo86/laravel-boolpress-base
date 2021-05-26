4be2b35dba371ae1acce1c28115fa39290080d27
Matteo <grecomatteo86@gmail.com>
2021-05-26 19:28:59 +0200

    creazione colonne pivot

--- database/migrations/2021_05_26_172254_create_post_tag_table.php
+++ database/migrations/2021_05_26_172254_create_post_tag_table.php
@@ -15,6 +15,8 @@ class CreatePostTagTable extends Migration
     {
         Schema::create('post_tag', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('post_id')->constrained();
+            $table->foreignId('tag_id')->constrained();
             $table->timestamps();
         });
     }
