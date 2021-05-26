d1bd25eb7db967d189e7da135d32500741a7b0c9
Matteo <grecomatteo86@gmail.com>
2021-05-26 17:44:03 +0200

    implementazione edit in PostController

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -89,9 +89,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Post $post)
     {
-        //
+        return view('admin.posts.edit', compact('post'));
     }
 
     /**
