e573c5b31ef47c2617682fd13d1c5a7706667651
Matteo <grecomatteo86@gmail.com>
2021-08-06 12:29:34 +0200

    final cleaning

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,13 +10,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', 'BlogController@index')->name('guest.posts.index');
 Route::get('posts/{slug}', 'BlogController@show')->name('guest.posts.show');
 Route::get('tags/{slug}', 'BlogController@filterTag')->name('guest.posts.filter-tag');
-
 Route::post('posts/{post}/add-comment', 'BlogController@addComment')->name('guest.posts.add-comment');
-
 Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function(){
     Route::resource('posts', 'PostController');
     Route::delete('comments/{comment}', 'CommentController@destroy')->name('comments.destroy');
